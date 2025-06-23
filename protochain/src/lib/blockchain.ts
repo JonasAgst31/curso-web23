ac911bca7223e2d8aa0fbb327d3b8718c65a7487
JonasAgst31 <jonas.a.santos@hotmail.com>
2025-06-23 14:10:43 -0300

    iniciando implementação da mempool

--- protochain/src/lib/blockchain.ts
+++ protochain/src/lib/blockchain.ts
@@ -7,11 +7,15 @@ import TransactionType from './transactionType';
 /**
  * Blockchain class
  */
+
 export default class Blockchain {
     blocks: Block[];
+    mempool: Transaction[];
     nextIndex: number = 0;
+
     static readonly DIFFICULTY_FACTOR = 5;
     static readonly MAX_DIFFICULTY = 62;
+    static readonly TX_PER_BLOCK = 2;
 
     /**
      * Creates a new blockchain
@@ -25,6 +29,7 @@ export default class Blockchain {
                 data: new Date().toString()
             } as Transaction)]
         } as Block)];
+        this.mempool = [];
         this.nextIndex++;
     }
 
@@ -36,6 +41,21 @@ export default class Blockchain {
         return Math.ceil(this.blocks.length / Blockchain.DIFFICULTY_FACTOR)
     }
 
+    addTransaction(transaction: Transaction): Validation {
+        const validation = transaction.isValid();
+        if (!validation.success)
+            return new Validation(false, "Invalid tx: " + validation.message)
+
+        if (this.blocks.some(b => b.transactions.some(tx => tx.hash === transaction.hash)))
+            return new Validation(false, "Duplicated tx in blockchain.");
+
+        if (this.mempool.some(tx => tx.hash === transaction.hash))
+            return new Validation(false, "Duplicated tx in mempool.");
+
+        this.mempool.push(transaction);
+        return new Validation(true, transaction.hash);
+    }
+
     addBlock(block: Block): Validation {
         const lastBlock = this.getLastBlock();
 
@@ -45,7 +65,14 @@ export default class Blockchain {
         this.blocks.push(block);
         this.nextIndex++;
 
-        return new Validation();
+        const txs = block.transactions.filter(tx => tx.type !== TransactionType.FEE).map(tx => tx.hash);
+        const newMempool = this.mempool.filter(tx => !txs.includes(tx.hash));
+        if (newMempool.length + txs.length !== this.mempool.length)
+            return new Validation(false, "Invalid tx in block: mempool.");
+
+        this.mempool = newMempool;
+
+        return new Validation(true, block.hash);
     }
 
     getBlock(hash: string): Block | undefined {
@@ -72,11 +99,11 @@ export default class Blockchain {
         return 1;
     }
 
-    getNextBlock(): BlockInfo {
-        const transactions = [new Transaction({
-            data: new Date().toString()
-        } as Transaction)];
+    getNextBlock(): BlockInfo | null {
+        if (!this.mempool || !this.mempool.length)
+            return null;
 
+        const transactions = this.mempool.slice(0, Blockchain.TX_PER_BLOCK);
         const difficulty = this.getDifficulty();
         const previousHash = this.getLastBlock().hash;
         const index = this.blocks.length;
